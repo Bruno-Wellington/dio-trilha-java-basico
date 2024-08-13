9c77a5a214d7ee1d4ecc4fa1c37b8cc21cafc7a1
Bruno Wellington <brunocarvalho.desenv@gmail.com>
2024-08-13 13:48:53 -0300

    Desafio ContaBanco finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+    
+        System.out.println("Por favor, digite o número da Agência:");
+        numero = sc.nextInt();
+
+        System.out.println("Por favor, digite a Agência:");
+        agencia = sc.next();
+        sc.nextLine();
+
+        System.out.println("Por favor, digite o seu nome:");
+        nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo:");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+            + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+        sc.close();
     }
 }
