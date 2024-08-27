117892fe3ce38ce2468d214526c73d5831fbf1d9
Bruno Wellington <brunocarvalho.desenv@gmail.com>
2024-08-27 11:11:12 -0300

    Exercicio 1 - Pesquisa em List

--- collections-java-api/src/List/PesquisaEmList/SomaDeNumeros/SomaNumeros.java
+++ collections-java-api/src/List/PesquisaEmList/SomaDeNumeros/SomaNumeros.java
@@ -1,2 +1,65 @@
-package List.PesquisaEmList.SomaDeNumeros;public class SomaNumeros {
+package List.PesquisaEmList.SomaDeNumeros;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomaNumeros {
+    private List<Integer> numeros;
+
+    public SomaNumeros() {
+        this.numeros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero){
+        this.numeros.add(numero);
+    }
+
+    public int calcularSoma(){
+        int soma = 0;
+        for(Integer n : numeros){
+            soma = n + soma;
+        }
+        return soma;
+    }
+
+    public int encontrarMaiorNumero(){
+        int maiorNumero = Integer.MIN_VALUE;
+        if(!numeros.isEmpty()){
+            for(Integer numero : numeros){
+                if(numero >= maiorNumero){
+                    maiorNumero = numero;
+                }
+            }
+            return maiorNumero;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public int encontrarMenorNumero(){
+        int menorNumero = Integer.MAX_VALUE;
+        if(!numeros.isEmpty()){
+            for(Integer numero : numeros) {
+                if (numero <= menorNumero) {
+                    menorNumero = numero;
+                }
+            }
+            return menorNumero;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirNumeros(){
+        if(!numeros.isEmpty()){
+            System.out.println(this.numeros);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return numeros + ", ";
+    }
 }
--- collections-java-api/src/List/PesquisaEmList/SomaDeNumeros/TesteList.java
+++ collections-java-api/src/List/PesquisaEmList/SomaDeNumeros/TesteList.java
@@ -1,2 +1,28 @@
-package List.PesquisaEmList.SomaDeNumeros;public class TesteList {
+package List.PesquisaEmList.SomaDeNumeros;
+
+public class TesteList {
+    public static void main(String[] args) {
+        // Criando uma instância da classe SomaNumeros
+        SomaNumeros somaNumeros = new SomaNumeros();
+
+        // Adicionando números à lista
+        somaNumeros.adicionarNumero(10);
+        somaNumeros.adicionarNumero(7);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(-5);
+        somaNumeros.adicionarNumero(3);
+
+        // Exibindo a lista de números adicionados
+        System.out.println("Números adicionados:");
+        somaNumeros.exibirNumeros();
+
+        // Calculando e exibindo a soma dos números na lista
+        System.out.println("Soma dos números = " + somaNumeros.calcularSoma());
+
+        // Encontrando e exibindo o maior número na lista
+        System.out.println("Maior número = " + somaNumeros.encontrarMaiorNumero());
+
+        // Encontrando e exibindo o menor número na lista
+        System.out.println("Menor número = " + somaNumeros.encontrarMenorNumero());
+    }
 }
