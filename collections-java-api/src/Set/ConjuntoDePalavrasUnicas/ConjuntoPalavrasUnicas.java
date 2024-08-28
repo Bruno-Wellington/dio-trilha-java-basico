111fef2eab2631dc314e1fd692ccfeec96aba762
Bruno Wellington <brunocarvalho.desenv@gmail.com>
2024-08-28 16:00:29 -0300

    Exercicio 2 - Operações Básicas com Set, criando um conjunto de palavras.

---
+++ collections-java-api/src/Set/ConjuntoDePalavrasUnicas/ConjuntoPalavrasUnicas.java
@@ -0,0 +1,61 @@
+package Set.ConjuntoDePalavrasUnicas;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoPalavrasUnicas {
+    private Set<String> palavrasUnicasSet;
+
+    public ConjuntoPalavrasUnicas() {
+        this.palavrasUnicasSet = new HashSet<>();
+    }
+
+    public void adicionarPalavra(String palavra){
+        palavrasUnicasSet.add(palavra);
+    }
+
+    /*public void removerPalavra(String palavra){
+        String palavraParaRemover = null;
+        for(String p : palavrasUnicasSet){
+            if(p.equalsIgnoreCase(palavra) ){
+                palavraParaRemover = p;
+                break;
+            }
+        }
+        palavrasUnicasSet.remove(palavraParaRemover);
+    }
+    */
+
+    //correção removerPalavra
+    public void removerPalavra(String palavra){
+        if (palavrasUnicasSet.contains(palavra)) {
+            palavrasUnicasSet.remove(palavra);
+        } else {
+            System.out.println("Palavra não encontrada no conjunto!");
+        }
+    }
+
+    /*
+    //Esse metodo que fiz ele compara melhor pois ele iginora a forma que foi escrita a palavra
+    public boolean verificarPalavra(String palavra){
+        boolean palavraVerificadar = false;
+        for(String p : palavrasUnicasSet) {
+            if (p.equalsIgnoreCase(palavra)) {
+                palavraVerificadar = true;
+            }
+        }
+        return palavraVerificadar;
+    }
+    */
+
+    //Correção verificarPalavra
+    public boolean verificarPalavra(String palavra) {
+        return palavrasUnicasSet.contains(palavra);
+    }
+
+
+    public void exibirPalavrasUnicas(){
+        System.out.println(palavrasUnicasSet);
+    }
+}
---
+++ collections-java-api/src/Set/ConjuntoDePalavrasUnicas/TesteColecaoSet.java
@@ -0,0 +1,26 @@
+package Set.ConjuntoDePalavrasUnicas;
+
+public class TesteColecaoSet {
+    public static void main(String[] args) {
+        ConjuntoPalavrasUnicas conjuntoPalavrasUnicas = new ConjuntoPalavrasUnicas();
+
+        //Adicionando uma palavra no conjunto
+        System.out.println("Adicionando palavras");
+        conjuntoPalavrasUnicas.adicionarPalavra("Amor");
+        conjuntoPalavrasUnicas.adicionarPalavra("Tempo");
+        conjuntoPalavrasUnicas.adicionarPalavra("Morte");
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+
+        //Removendo uma palavra do conjunto
+        System.out.println();
+        System.out.println("Removendo palavra");
+        conjuntoPalavrasUnicas.removerPalavra("Tempo");
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+
+        //Verificando se a palavra esta no conjunto
+        System.out.println();
+        conjuntoPalavrasUnicas.verificarPalavra("amor");
+        System.out.println("A palavra 'Tempo' esta no conjunto? " + conjuntoPalavrasUnicas.verificarPalavra("Tempo"));
+        System.out.println("A palavra 'Amor' esta no conjunto? " + conjuntoPalavrasUnicas.verificarPalavra("Amor"));
+    }
+}
