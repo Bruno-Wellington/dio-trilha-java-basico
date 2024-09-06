41b65fdf4bf598a5e86980279042e34d4c0e20f6
Bruno Wellington <brunocarvalho.desenv@gmail.com>
2024-09-06 17:09:56 -0300

    Desafio bootcamp poo - Testando Instancias na classe main

--- desafio-dio-bootcamp-poo/src/Main.java
+++ desafio-dio-bootcamp-poo/src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -25,9 +27,43 @@ public class Main {
         mentoria2.setDescricao("Descrição mentoria do curso C#");
         mentoria2.setData(LocalDate.now());//LocalDate.now() atribui a data de criação á data atual.
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
-        System.out.println(mentoria2);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria1);
+//        System.out.println(mentoria2);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição bootcamp java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devBruno = new Dev();
+        devBruno.setNome("Bruno Wellington");
+        devBruno.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Incritos de " + devBruno.getNome() + ": " + devBruno.getConteudosInscritos());
+
+        devBruno.progredir();
+        devBruno.progredir();
+        System.out.println("---");
+        System.out.println("Conteudos Incritos de " + devBruno.getNome() + ": " + devBruno.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos de " + devBruno.getNome() + ": " + devBruno.getConteudosConcluidos());
+        System.out.println("XP:" + devBruno.calcularTotalXp());
+
+
+        System.out.println("----------------------------------------------------------");
+        Dev devEvandro = new Dev();
+        devEvandro.setNome("Evandro Marques");
+        devEvandro.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Incritos de " + devEvandro.getNome() + ": " + devEvandro.getConteudosInscritos());
+
+        devEvandro.progredir();
+        devEvandro.progredir();
+        devEvandro.progredir();
+        System.out.println("---");
+        System.out.println("Conteudos Incritos de " + devEvandro.getNome() + ": " + devEvandro.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos de " + devEvandro.getNome() + ": " + devEvandro.getConteudosConcluidos());
+        System.out.println("XP:" + devEvandro.calcularTotalXp());
     }
 }
--- desafio-dio-bootcamp-poo/src/br/com/dio/desafio/dominio/Dev.java
+++ desafio-dio-bootcamp-poo/src/br/com/dio/desafio/dominio/Dev.java
@@ -4,6 +4,7 @@ import com.sun.source.doctree.SeeTree;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -12,15 +13,25 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp){
-
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir(){
-
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
     }
 
-    public void calcularTotalXp(){
-
+    public double calcularTotalXp(){
+       return this.conteudosConcluidos
+               .stream()
+               .mapToDouble(conteudo -> conteudo.calculaXp())
+               .sum();
     }
 
     public String getNome() {
