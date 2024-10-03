3e64aa2b34ee4afa0630cc2d04a38be9427ce055
Bruno Wellington <brunocarvalho.desenv@gmail.com>
2024-10-03 15:02:18 -0300

    primeiros passos spring boot

---
+++ Primeiros-passos-springboot/src/main/java/dio/springboot/PrimeirosPassosSpringbootApplication.java
@@ -0,0 +1,12 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirosPassosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirosPassosSpringbootApplication.class, args);
+	}
+}
