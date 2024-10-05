65972bf2f0d8f25fc74ab71ff123068bdfb0a461
Bruno Wellington <brunocarvalho.desenv@gmail.com>
2024-10-05 11:09:13 -0300

    Explicações sobre JPA Repository

--- Primeiros-passos-spring-data-jpa/src/main/java/dio/springdatajpa/StartApp.java
+++ Primeiros-passos-spring-data-jpa/src/main/java/dio/springdatajpa/StartApp.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
     @Autowired
@@ -13,15 +15,27 @@ public class StartApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user = new User();
-        user.setNome("Bruno Wellington");
-        user.setUsername("bruno");
-        user.setPassword("1234");
+//        User user = new User();
+//        user.setNome("Bruno Wellington");
+//        user.setUsername("bruno");
+//        user.setPassword("1234");
 
-        repository.save(user);
-
-        for(User u : repository.findAll()){
+        List<User> users = repository.findByNomeContaining("Bruno");
+        for(User u : users){
             System.out.println(u);
         }
+    }
+
+    private void insertUser(){
+        User user = new User();
+        user.setNome("Bruno");
+        user.setUsername("Bruno");
+        user.setPassword("carvalho");
+        repository.save(user);
+
+        for(User u : repository.findAll()) {
+            System.out.println(u);
+        }
+
     }
 }
--- Primeiros-passos-spring-data-jpa/src/main/java/dio/springdatajpa/repository/UserRepository.java
+++ Primeiros-passos-spring-data-jpa/src/main/java/dio/springdatajpa/repository/UserRepository.java
@@ -2,7 +2,20 @@ package dio.springdatajpa.repository;
 
 import dio.springdatajpa.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
 
+    //Query Method
+    List<User> findByNomeContaining(String nome);
+
+    //Query Method
+    User findByUsername(String username);
+
+    //Query Override
+    @Query("SELECT u FROM User u WHERE u.nome LIKE %:nome%")
+    List<User> filtrarPorNome(@Param("nome") String nome);
 }
